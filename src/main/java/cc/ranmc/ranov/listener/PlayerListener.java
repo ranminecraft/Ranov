b6f3942a8d1780a69e396ad49c480c91703b3050
Ran <xyfwdy@gmail.com>
2025-11-18 01:06:32 +0800

    玩家加入的时候传送到配置文件的大厅

--- src/main/java/cc/ranmc/ranov/listener/PlayerListener.java
+++ src/main/java/cc/ranmc/ranov/listener/PlayerListener.java
@@ -1,12 +1,22 @@
 package cc.ranmc.ranov.listener;
 
 import cc.ranmc.ranov.Main;
+import cc.ranmc.ranov.util.BasicUtil;
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // 玩家加入服务器传送到大厅
+        Location location = BasicUtil.getLocation(Main.getInstance().getConfig().getString("lobby-location"));
+        if (location != null) event.getPlayer().teleport(location);
+    }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
