69bae0f649f3009ff259b5a00b492f614227abe4
Ran <xyfwdy@gmail.com>
2025-11-26 09:15:29 +0800

    死亡箱子消失

--- src/main/java/cc/ranmc/ranov/listener/PlayerListener.java
+++ src/main/java/cc/ranmc/ranov/listener/PlayerListener.java
@@ -6,13 +6,20 @@ import cc.ranmc.ranov.util.BasicUtil;
 import cc.ranmc.ranov.util.GameUtil;
 import cc.ranmc.ranov.util.TeamUtil;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Chest;
+import org.bukkit.block.DoubleChest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.DoubleChestInventory;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerListener implements Listener {
 
@@ -21,6 +28,40 @@ public class PlayerListener implements Listener {
         // 玩家加入服务器传送到大厅
         Location location = BasicUtil.getLocation(Main.getInstance().getConfig().getString("lobby-location"));
         if (location != null) event.getPlayer().teleport(location);
+    }
+
+    @EventHandler
+    public void onInventoryCloseEvent(InventoryCloseEvent event) {
+        Inventory inv = event.getInventory();
+
+        if (!(inv instanceof DoubleChestInventory)) return;
+
+        DoubleChestInventory dc = (DoubleChestInventory) inv;
+
+        // 判断双箱是否完全空
+        if (isEmpty(dc)) {
+            DoubleChest holder = dc.getHolder();
+
+            // 获取左、右两个 Chest 方块
+            Chest left = (Chest) holder.getLeftSide();
+            Chest right = (Chest) holder.getRightSide();
+
+            if (left != null && left.getBlock() != null && left.getCustomName().endsWith(" 的死亡物品")) {
+                left.getBlock().setType(Material.AIR);
+            }
+            if (right != null && right.getBlock() != null && right.getCustomName().endsWith(" 的死亡物品")) {
+                right.getBlock().setType(Material.AIR);
+            }
+        }
+    }
+
+    private boolean isEmpty(DoubleChestInventory inv) {
+        for (ItemStack item : inv.getContents()) {
+            if (item != null && item.getType() != Material.AIR) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @EventHandler
